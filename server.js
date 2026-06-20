0b0fd5585d998c8df93261b81d720587466eccd3
7lol7 <7lol7mc@gmail.com>
2026-06-20 16:31:43 -0400

    Update server.js

--- server.js
+++ server.js
@@ -8,17 +8,15 @@ const io = require('socket.io')(http, {
     }
 });
 
-// Use the port provided by the hosting platform, or default to 3000 locally
-// Serve the index.html file
+const PORT = process.env.PORT || 3000;
+
 const path = require('path');
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Serve your other files (like css/js if they are in folders)
 app.use(express.static(__dirname));
 
-// Start the server
 http.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
